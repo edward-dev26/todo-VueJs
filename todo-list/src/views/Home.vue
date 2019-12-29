0953e5a6d49421643ed5a8569d906acd596bd0ef
Eduard Shvetsov <edward.sh.itlt@gmail.com>
2019-12-29 12:22:43 +0100

    Initial commit

---
+++ todo-list/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="home">
+        <h1>This a home page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur culpa eaque minus non porro
+            praesentium, quam quo ratione, repellendus rerum sapiente similique sit ullam? Dolor ducimus,
+            voluptatibus. Exercitationem, fugiat, necessitatibus?</p>
+        <a href="/todo">ToDo list</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+    .home {
+        text-align: center;
+        width: 100%;
+        max-width: 1200px;
+        margin: auto;
+    }
+</style>
---
+++ todo-list/src/views/ToDoList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="todo">
+        <ul class="todo__list">
+            <li>To do</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ToDoList"
+    }
+</script>
+
+<style scoped>
+
+</style>
